365c3a57bc89af3efd1e4aca2981b8b0be8dffe1
sailesh.cet06@gmail.com <sailesh.cet06@gmail.com>
2022-08-16 09:34:59 +0000

    added seed

--- models/staging/stg_orders.sql
+++ models/staging/stg_orders.sql
@@ -1,8 +1,10 @@
 select 
+{{ dbt_utils.surrogate_key(['o.orderid', 'c.customerid', 'p.productid']) }} as surrogatekey,
 o.orderId,
 o.orderdate,
 o.shipdate,
 o.shipmode,
+d.delivery_team,
 o.ordersellingprice - o.ordercostprice as orderprofit,
 o.ordersellingprice,
 o.ordercostprice,
@@ -13,9 +15,12 @@ c.country,
 p.productid,
 p.catagory,
 p.productname,
-p.subcatagory
+p.subcatagory,
+{{markup('ordersellingprice', 'ordercostprice')}} as markup
 from {{ ref('raw_orders') }} as o
 left join {{ ref('raw_customer') }} as c on 
 o.customerid = c.customerid
 left join {{ ref('raw_product') }} as p on
 o.productid = p.productid
+left join {{ ref('delivery_team') }} as d on
+d.shipmode = o.shipmode
